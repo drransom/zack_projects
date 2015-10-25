718590dbf5a62022e5cf1174959b91e162b8f8ee
Elliot Reed <elliotreed2@gmail.com>
2015-10-25 15:55:13 -0700

    able to compare correllation against training data

--- data_analyzer.py
+++ data_analyzer.py
@@ -1,5 +1,7 @@
 import numpy
 import os
+import scipy
+from scipy import stats
 import sklearn
 from sklearn import linear_model
 from sklearn.ensemble import RandomForestClassifier
@@ -39,30 +41,48 @@ def regress_from_data(independent, dependent, maxlines = False):
     # LinearRegression(copy_X=True, fit_intercept=True, n_jobs=1, normalize=False))
     return clf
 
-def regress_and_predict_from_files(independent, dependent, num_training = False):
-    independent = import_numerical_data(independent)
-    dependent = import_1d_data(dependent)
+def regress_and_predict_from_files(independent, dependent, num_training = False, end = False):
     if not num_training:
         num_training = len(independent)
-    clf = CLF(independent, dependent)
+    clf = DataAnalyzer(independent, dependent)
     clf.fit(num_training)
-    return clf.predict(num_training)
+    return clf.predict(num_training, end)
 
-class CLF:
+class DataAnalyzer:
     def __init__(self, independent, dependent):
+        self.independent = import_numerical_data(independent)
+        self.dependent = import_1d_data(dependent)
         self.clf = linear_model.LinearRegression()
-        self.independent = independent
-        self.dependent = dependent
 
-    def fit(self, maxlines):
+    def fit(self, maxlines = False):
+        if not maxlines:
+            maxlines = len(self.dependent)
         independent = self.independent[:maxlines]
         dependent = self.dependent[:maxlines]
         self.clf.fit(independent, dependent)
 
-    def predict(self, start):
-        return self.clf.predict(self.independent[start:])
+    def predict(self, start, end = False):
+        if end:
+            return self.clf.predict(self.independent[start:(end+1)])
+        else:
+            return self.clf.predict(self.independent[start:])
+
+    def correllation(self, start = 0, end = False):
+        if not end:
+            end = len(self.dependent)
+        self.fit(end)
+        predicted = self.predict(start, end)
+        return stats.pearsonr(predicted, self.dependent[start:end])
+
+    def correllation_validate(self, num_training):
+        self.fit(num_training)
+        predicted = self.predict(num_training)
+        return stats.pearsonr(predicted, self.dependent[num_training:])
 
 
-filename1 = './MachineLearningASCIIData/LARain1901_2010.txt'
-filename2 = './MachineLearningASCIIData/TBOT_Alphas.txt'
-print regress_and_predict_from_files(filename2, filename1, 60)
+if __name__ == "__main__":
+    filename1 = './MachineLearningASCIIData/LARain1901_2010.txt'
+    filename2 = './MachineLearningASCIIData/TBOT_Alphas.txt'
+    analyzer = DataAnalyzer(filename2, filename1)
+    # analyzer.fit()
+    print analyzer.correllation_validate(60)
